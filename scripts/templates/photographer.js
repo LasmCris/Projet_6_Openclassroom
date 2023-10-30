04cbd58b7d56b8e103212116f51460d73183581a
LasmCris <lasmchristian@gmail.com>
2023-10-30 23:14:51 +0100

    Nouvelle configuration Eslint

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,71 +1,69 @@
 function photographerTemplate(data) {
-	const { name, portrait, country, city, tagline, price, id } = data;
+    const { name, portrait, country, city, tagline, price, id } = data;
 
-	const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${portrait}`;
     
 
-	//Cette fonction a pour but de retourner le parent (1er parent) de tous les enfants creér
-	function getUserCardDOM() {
-		const article = document.createElement("article");
-		const figure = document.createElement("figure");
-		figure.classList.add("figurePhotographe");
-		article.appendChild(figure);
-		const lienPhoto = document.createElement("a");
-		lienPhoto.setAttribute("href", `./photographer.html?id=${id}`);
-		lienPhoto.classList.add("lienPhotoProfil");
-		const figcaption = document.createElement("figcaption");
-		figcaption.classList.add("figcaptionPhotographe");
-		figure.appendChild(lienPhoto);
-		figure.appendChild(figcaption);
+    //Cette fonction a pour but de retourner le parent (1er parent) de tous les enfants creér
+    function getUserCardDOM() {
+      const article = document.createElement("article");
+      const figure = document.createElement("figure");
+      figure.classList.add("figurePhotographe");
+      article.appendChild(figure);
+      const lienPhoto = document.createElement("a");
+      lienPhoto.setAttribute("href", `./photographer.html?id=${id}`);
+      lienPhoto.classList.add("lienPhotoProfil");
+      const figcaption = document.createElement("figcaption");
+      figcaption.classList.add("figcaptionPhotographe");
+      figure.appendChild(lienPhoto);
+      figure.appendChild(figcaption);
 
-		const img = document.createElement("img");
-		img.classList.add("photoProfil");
-		img.setAttribute("src", picture);
-		// Ajout d'un attribut ARIA pour donner une description de l'image
-		img.setAttribute("alt", `Photo de profil de ${name}`);
+      const img = document.createElement("img");
+      img.classList.add("photoProfil");
+      img.setAttribute("src", picture);
+      // Ajout d'un attribut ARIA pour donner une description de l'image
+      img.setAttribute("alt", `Photo de profil de ${name}`);
 
-		lienPhoto.appendChild(img);
-		// Ajout d'un attribut ARIA pour indiquer que c'est un lien
-		lienPhoto.setAttribute("role", "link");
-		// Ajout d'un attribut ARIA pour donner un nom au lien
-		lienPhoto.setAttribute("aria-label", `Voir le profil de ${name}`);
+      lienPhoto.appendChild(img);
+      // Ajout d'un attribut ARIA pour indiquer que c'est un lien
+      lienPhoto.setAttribute("role", "link");
+      // Ajout d'un attribut ARIA pour donner un nom au lien
+      lienPhoto.setAttribute("aria-label", `Voir le profil de ${name}`);
 
-		const h2Name = document.createElement("h2");
-		h2Name.textContent = name;
-		h2Name.classList.add("nomPhotographe");
+      const h2Name = document.createElement("h2");
+      h2Name.textContent = name;
+      h2Name.classList.add("nomPhotographe");
 
-		const divCountryCity = document.createElement("div");
-		divCountryCity.classList.add("countryCityDiv");
+      const divCountryCity = document.createElement("div");
+      divCountryCity.classList.add("countryCityDiv");
 
-		const h3Country = document.createElement("h3");
-		h3Country.textContent = country;
-		h3Country.classList.add("country");
+      const h3Country = document.createElement("h3");
+      h3Country.textContent = country;
+      h3Country.classList.add("country");
 
-		const h3City = document.createElement("h3");
-		h3City.textContent = city;
-		h3City.classList.add("city");
+      const h3City = document.createElement("h3");
+      h3City.textContent = city;
+      h3City.classList.add("city");
 
-		const pTagLine = document.createElement("p");
-		pTagLine.textContent = tagline;
-		pTagLine.classList.add("tagLine");
+      const pTagLine = document.createElement("p");
+      pTagLine.textContent = tagline;
+      pTagLine.classList.add("tagLine");
 
-		const pPrice = document.createElement("p");
-		pPrice.textContent = `${price} / jours`;
-		pPrice.classList.add("prix");
+      const pPrice = document.createElement("p");
+      pPrice.textContent = `${price} / jours`;
+      pPrice.classList.add("prix");
 
-		figcaption.appendChild(h2Name);
-		figcaption.appendChild(divCountryCity);
-		divCountryCity.appendChild(h3Country);
-		divCountryCity.appendChild(h3City);
-		figcaption.appendChild(pTagLine);
-		figcaption.appendChild(pPrice);
+      figcaption.appendChild(h2Name);
+      figcaption.appendChild(divCountryCity);
+      divCountryCity.appendChild(h3Country);
+      divCountryCity.appendChild(h3City);
+      figcaption.appendChild(pTagLine);
+      figcaption.appendChild(pPrice);
 
-		return article;
-	}
+      return article;
+    }
 
-	return { getUserCardDOM }; //Cela signifie que la fonction photographerTemplate() 
-	//retourne le resultat de la fonction getUserCardDOM
+    return { getUserCardDOM } //Cela signifie que la fonction photographerTemplate() 
+    //retourne le resultat de la fonction getUserCardDOM
 
 }
-
-export default photographerTemplate;
--- scripts/utils/lightBox.js
+++ scripts/utils/lightBox.js
@@ -1,120 +1,120 @@
 function lightBox () {
-	const lightBox = document.querySelector("#lightBox");
-	const liensPhotos = document.querySelectorAll(".lienMedia__media");
-	const main1 = document.querySelector("main");
+  const lightBox = document.querySelector("#lightBox");
+  const liensPhotos = document.querySelectorAll(".lienMedia__media");
+  const main1 = document.querySelector("main");
 
-	const arrayPhotos = Array.from(liensPhotos);
-	console.log(arrayPhotos);
-	const imageDansLightbox = lightBox.querySelector(".lightBoxContent img");
-	const videoDansLightbox = lightBox.querySelector(".lightBoxContent video");
-	const prevButton = document.querySelector(".lightBoxPrev");
-	const nextButton = document.querySelector(".lightBoxNext");
-	const close1 = document.querySelector(".close");
+  const arrayPhotos = Array.from(liensPhotos);
+  console.log(arrayPhotos);
+  const imageDansLightbox = lightBox.querySelector(".lightBoxContent img");
+  const videoDansLightbox = lightBox.querySelector(".lightBoxContent video");
+  const prevButton = document.querySelector(".lightBoxPrev");
+  const nextButton = document.querySelector(".lightBoxNext");
+  const close1 = document.querySelector(".close");
 
-	let currentIndex = 0;
+  let currentIndex = 0;
 
-	//fonction d'ouverture de la lightbox avec une image donnée.
+  //fonction d'ouverture de la lightbox avec une image donnée.
 
-	function ouvertureLightbox(index) {
-		const media = arrayPhotos[index];
+  function ouvertureLightbox(index) {
+    const media = arrayPhotos[index];
 
-		if (media.tagName === "IMG") {
-			imageDansLightbox.src = arrayPhotos[index].src;
-			imageDansLightbox.alt = arrayPhotos[index].alt;
-			videoDansLightbox.style.display = "none";
-			imageDansLightbox.style.display = "block";
-		} else if (media.tagName === "VIDEO") {
-			videoDansLightbox.style.display = "block";
-			imageDansLightbox.style.display = "none";
-			videoDansLightbox.setAttribute("controls", "true");
-			videoDansLightbox.querySelector("source").src =
+    if (media.tagName === "IMG") {
+      imageDansLightbox.src = arrayPhotos[index].src;
+      imageDansLightbox.alt = arrayPhotos[index].alt;
+      videoDansLightbox.style.display = "none";
+      imageDansLightbox.style.display = "block";
+    } else if (media.tagName === "VIDEO") {
+      videoDansLightbox.style.display = "block";
+      imageDansLightbox.style.display = "none";
+      videoDansLightbox.setAttribute("controls", "true");
+      videoDansLightbox.querySelector("source").src =
         arrayPhotos[index].querySelector("source").src;
 
-			// Charge la vidéo et lance la lecture automatique
-			videoDansLightbox.load();
-			videoDansLightbox.play();
-		}
+      // Charge la vidéo et lance la lecture automatique
+      videoDansLightbox.load();
+      videoDansLightbox.play();
+    }
 
-		currentIndex = index;
-		lightBox.classList.add("show");
-		//Pour les technologie d'assistance, a l'ouverture de la lightBox, l'ensemble de la
-		//page web doit etre caché, c'est ce que fait le code suivant
-		main1.ariaHidden = true;
-	}
+    currentIndex = index;
+    lightBox.classList.add("show");
+    //Pour les technologie d'assistance, a l'ouverture de la lightBox, l'ensemble de la
+    //page web doit etre caché, c'est ce que fait le code suivant
+    main1.ariaHidden = true;
+  }
 
-	function fermetureLightbox() {
-		lightBox.classList.remove("show");
-		//A la fermuture de la lightbox, il fut indique aux technologie d'assistance que
-		//Que le reste de la page est desormais visible avec le code suivant
-		main1.ariaHidden = false;
-	}
+  function fermetureLightbox() {
+    lightBox.classList.remove("show");
+    //A la fermuture de la lightbox, il fut indique aux technologie d'assistance que
+    //Que le reste de la page est desormais visible avec le code suivant
+    main1.ariaHidden = false;
+  }
 
-	//gerer la navigation vers l'image precedente
-	function imagePrecedente() {
-		currentIndex = (currentIndex - 1 + arrayPhotos.length) % arrayPhotos.length;
-		ouvertureLightbox(currentIndex);
-	}
+  //gerer la navigation vers l'image precedente
+  function imagePrecedente() {
+    currentIndex = (currentIndex - 1 + arrayPhotos.length) % arrayPhotos.length;
+    ouvertureLightbox(currentIndex);
+  }
 
-	//gerer la navigation vers l'image suivante
-	function imageSuivante() {
-		currentIndex = (currentIndex + 1) % arrayPhotos.length;
-		ouvertureLightbox(currentIndex);
-	}
+  //gerer la navigation vers l'image suivante
+  function imageSuivante() {
+    currentIndex = (currentIndex + 1) % arrayPhotos.length;
+    ouvertureLightbox(currentIndex);
+  }
 
-	//placer les ecouteurs d'evements click
-	arrayPhotos.forEach((lien, index) => {
-		lien.addEventListener("click", (e) => {
-			e.preventDefault();
-			ouvertureLightbox(index);
-		});
+  //placer les ecouteurs d'evements click
+  arrayPhotos.forEach((lien, index) => {
+    lien.addEventListener("click", (e) => {
+      e.preventDefault();
+      ouvertureLightbox(index);
+    });
 
-		lien.addEventListener("keypress", (e) => {
-			if (e.key === "Enter" || e.code === "Enter") {
-				e.preventDefault();
-				ouvertureLightbox(index);
-			}
-		});
-	});
+    lien.addEventListener("keypress", (e) => {
+      if (e.key === "Enter" || e.code === "Enter") {
+        e.preventDefault();
+        ouvertureLightbox(index);
+      }
+    });
+  });
 
-	prevButton.addEventListener("click", imagePrecedente);
-	prevButton.addEventListener("keydown", (e) => {
-		if (e.key === "ArrowLeft" || e.code === "ArrowLeft") {
-			e.preventDefault();
-			imagePrecedente();
-		}
-	});
+  prevButton.addEventListener("click", imagePrecedente);
+  prevButton.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft" || e.code === "ArrowLeft") {
+      e.preventDefault();
+      imagePrecedente();
+    }
+  });
   
 
-	nextButton.addEventListener("click", imageSuivante);
-	nextButton.addEventListener("keydown", (e) => {
-		if (e.key === "ArrowRight" || e.code === "ArrowRight") {
-			e.preventDefault();
-			imageSuivante();
-		}
-	});
+  nextButton.addEventListener("click", imageSuivante);
+  nextButton.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight" || e.code === "ArrowRight") {
+      e.preventDefault();
+      imageSuivante();
+    }
+  });
 
 
 
-	close1.addEventListener("click", fermetureLightbox);
-	close1.addEventListener("keydown", (e) => {
-		if (e.key === "Escape" || e.code === "Escape") {
-			e.preventDefault();
-			fermetureLightbox();
-		}
-	});
+  close1.addEventListener("click", fermetureLightbox);
+  close1.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" || e.code === "Escape") {
+      e.preventDefault();
+      fermetureLightbox();
+    }
+  });
 
 
 
-	// Ajout d'un écouteur d'événement pour la touche "Esc"
-	document.addEventListener("keydown", function (event) {
-		if (event.key === "Escape") {
-			fermetureLightbox();
-		}
-	});
+  // Ajout d'un écouteur d'événement pour la touche "Esc"
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      fermetureLightbox();
+    }
+  });
 
 
   
-}
+};
 
 
 export default lightBox;
